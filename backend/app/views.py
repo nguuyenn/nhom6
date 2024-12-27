32a537e775b7efae2a36983bf000c05e6f4e535e
Quang Trung <quangtrung54677@gmail.com>
2024-12-28 04:35:45 +0700

    Django Admin Set list display

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
-
+from .models import TinhThanh
 def Index(request):
     return render(request,'index.html')
 
@@ -17,5 +17,9 @@ def TuyenDung(request):
     template = loader.get_template('tuyendung.html')
     return HttpResponse(template.render())
 
+def TinhThanh(request):
+    return HttpResponse("")
 
+def TTDuAn(request):
+    return HttpResponse("")
 # Create your views here.
